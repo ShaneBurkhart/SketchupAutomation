efedb5ffd6971e9c7e156227d9bcba571528da2d
Shane Burkhart <shaneburkhart@gmail.com>
2019-01-22 21:43:59 -0600

    Added handler.

--- drawing_plugin/drawing_plugin/main.rb
+++ drawing_plugin/drawing_plugin/main.rb
@@ -1,6 +1,34 @@
 require "sketchup.rb"
+require "cgi"
 
 module FinishVisionVR
+    class ComponentLoadHandler
+        attr_accessor :error, :url
+
+        def initialize(url)
+            self.url = url
+        end
+
+        def onPercentChange(percent)
+            Sketchup::set_status_text("LOADING: #{percent}%")
+        end
+
+        def cancelled?
+            return false
+        end
+
+        def onSuccess
+            Sketchup::set_status_text('')
+            UI.messagebox("Handler loaded")
+        end
+
+        def onFailure(error_message)
+            self.error = error_message
+            Sketchup::set_status_text('')
+            UI.messagebox(error_message)
+        end
+    end
+
     module DrawingPlugin
         COMPONENT_SEARCH_URL = "http://construction-vr.shaneburkhart.com/93e8e03a-9c36-48bc-af15-54db7715ac15/component/search"
 
@@ -11,6 +39,7 @@ module FinishVisionVR
             )
             dialog.set_url COMPONENT_SEARCH_URL
             dialog.add_action_callback("add_to_model") { |action_context, url|
+                dialog.close
                 FinishVisionVR::DrawingPlugin.place_component(url)
             }
             dialog.show
@@ -18,8 +47,10 @@ module FinishVisionVR
 
         def self.place_component(url)
             model = Sketchup.active_model
-            comp = model.definitions.load_from_url(url)
+            comp = model.definitions.load_from_url(url, FinishVisionVR::ComponentLoadHandler.new(url))
+            UI.messagebox("Loaded")
             return if comp.nil?
+            UI.messagebox("Place that")
             Sketchup.active_model.place_component(comp)
         end
 
