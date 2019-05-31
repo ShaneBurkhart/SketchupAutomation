9647ba2f98513b1fb2aba0f05eceef4b52414b8c
Shane Burkhart <shaneburkhart@gmail.com>
2019-05-31 14:54:17 -0500

    Add lock to not send multiple requests.

--- drawing_plugin/drawing_plugin/main.rb
+++ drawing_plugin/drawing_plugin/main.rb
@@ -30,20 +30,38 @@ module FinishVisionVR
 
     module DrawingPlugin
         COMPONENT_SEARCH_URL = "http://construction-vr.shaneburkhart.com/93e8e03a-9c36-48bc-af15-54db7715ac15/component/search"
+        @@is_loading_component = false
+
+        # The name of the file should give us the record ID for the unit
+        def self.get_unit_id
+            model = Sketchup.active_model
+            title = model.title || ""
+            title_parts = title.split("-")
+            return nil if title_parts.length == 0
+            return title_parts[0].strip
+        end
 
         def self.open_component_search
+            unit_id = FinishVisionVR::DrawingPlugin.get_unit_id
+            return if unit_id.nil?
+
             dialog = UI::HtmlDialog.new(
                 width: 900,
                 height: 600,
             )
-            dialog.set_url COMPONENT_SEARCH_URL
+            dialog.set_url "#{COMPONENT_SEARCH_URL}?unit_id=#{unit_id}"
             dialog.add_action_callback("add_to_model") { |action_context, url, type|
+                next if @@is_loading_component
+                @@is_loading_component = true
+
                 UI.start_timer(1.0, false) {
-                    dialog.close
                     model = Sketchup.active_model
+                    model.select_tool('SelectionTool')
                     comp = FinishVisionVR::DrawingPlugin.load_component(url)
 
+                    model.select_tool('SelectionTool')
                     FinishVisionVR::DrawingPlugin.place_component(comp)
+                    @@is_loading_component = false
                 }
             }
             dialog.show
